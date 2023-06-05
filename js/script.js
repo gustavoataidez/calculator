43fe837fd1a03341dacd1ac92f1dc8ee19337d06
gustavoataidez <luisgustavoataidee@gmail.com>
2023-06-05 20:13:24 -0300

    Buttons

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const add = document.getElementById("add")
+const one = document.getElementById("one")
+const two = document.getElementById("two")
+const three = document.getElementById("three")
+
+function clickNumber(number){
+  alert(number)
+}
