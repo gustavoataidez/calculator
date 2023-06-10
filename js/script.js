eacf8c79fc89769e15f8ebf177404c8ca7a9e36a
gustavoataidez <luisgustavoataidee@gmail.com>
2023-06-10 03:51:39 -0300

    Change from label to input

--- js/script.js
+++ js/script.js
@@ -2,7 +2,27 @@ const add = document.getElementById("add")
 const one = document.getElementById("one")
 const two = document.getElementById("two")
 const three = document.getElementById("three")
+let conteudoResultado = document.getElementById("result-value").innerText;
+var descricao = document.getElementById("result-description")
 
+
+var numberglobal = []
+var calcular = []
 function clickNumber(number){
-  alert(number)
+  descricao.value += number
+  console.log(descricao)
+}
+function sum(a,b){
+  return a + b
+}
+
+function resultNumber(){
+  alert(eval(descricao))
+}
+
+function sum() {
+for (let i = 0; i < numberglobal.length; i++) {
+  var numero = numberglobal[i];
+  calcular.push(numero)
+}
 }
